590f58919d56df6eb6bbce97a22f409423cbfe15
Lucas <lucasdcaceres00@gmail.com>
2022-07-10 17:34:33 -0300

    preparing to merge

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -8,25 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logo.component.css']
 })
 export class LogoComponent implements OnInit {
-  config;
-  fullpage_api: any;
+ 
 
   constructor() {
 
-    // this is just an example => for more details on config please visit fullPage.js docs
-    this.config = {
-      licenseKey: 'GPLv3',
-      sectionsColor: ['#7BAABE', 'whitesmoke', '#7BAABE', 'whitesmoke', '#7BAABE'],
-      anchors: ['p1', 'p2', 'p3', 'p4', 'p5'],//Aca esta el URL de la pagina
-      navigation: true,
-    };
+   
   }
 
   ngOnInit() {
   }
 
-  getRef(fullPageRef: any) {
-    this.fullpage_api = fullPageRef;
-  }
 
 }
