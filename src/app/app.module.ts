e8beb47ef2f57ac70351cc9fadc0e2e6a4a8e9bd
Lucas <lucasdcaceres00@gmail.com>
2022-07-10 16:17:58 -0300

    Added fullpage.js wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFullpageModule
     
   ],
   providers: [],
